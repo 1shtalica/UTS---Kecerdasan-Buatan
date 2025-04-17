7d61ed906331c8cdab1c8ae8c94bce30ffdf45bf
1shtalica <adibfaizulhaq05@gmail.com>
2025-04-17 10:21:37 +0700

    Membuat desain program

---
+++ Genetic_Algorithm.py
@@ -0,0 +1,13 @@
+import random
+import math
+
+#Desain Program
+ukuranPopulasi = 100
+ukuranKromosom = 32
+maxGenerasi = 100
+crossOverRate = 0.8
+mutationRate = 1/ukuranKromosom
+elite_count = 1
+
+#Fungsi
+
